12d0f4c77b888374e51dd19ff7444b4f7dd63931
Raneem1993 <raneem.r7@hotmail.com>
2024-03-11 11:24:46 +0100

    components

--- Nachhilfe/src/app/app.module.ts
+++ Nachhilfe/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { StartseiteComponent } from './components/startseite/startseite.component';
+import { TableComponent } from './components/table/table.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StartseiteComponent,
+    TableComponent,
+    CardsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

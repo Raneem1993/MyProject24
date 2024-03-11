6c399891a9eadb41e013126c736816224e70a534
Raneem1993 <raneem.r7@hotmail.com>
2024-03-11 11:37:54 +0100

    new

--- Nachhilfe/src/app/components/startseite/startseite.component.ts
+++ Nachhilfe/src/app/components/startseite/startseite.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startseite',
   templateUrl: './startseite.component.html',
   styleUrls: ['./startseite.component.css']
 })
-export class StartseiteComponent {
+export class StartseiteComponent implements OnInit  {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+changeRoute(){
+  this.router.navigateByUrl('/anmeldung');
+}
 
 }
